b375c440a666d7a6b0425886e28fb28cd09638f1
Alex Kolov <alex@kolov.de>
2016-02-29 01:06:48 +0100

    Update dependencies & bumped up version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject de.kolov/dbg "0.1.0"
+(defproject de.kolov/dbg "0.2.0"
   :description "tiny debug lib"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [mvxcvi/puget "0.9.1"]])
+                 [mvxcvi/puget "1.0.0"]])
